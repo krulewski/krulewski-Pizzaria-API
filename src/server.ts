a25c99b5dceb35a82432305a7930b73a875992a3
krulewski <vinicius.delfino@fatec.sp.gov.br>
2026-04-20 18:06:37 -0300

    fix: libera CORS para frontend (Vercel)

--- src/server.ts
+++ src/server.ts
@@ -6,28 +6,24 @@ import cors from 'cors'
 
 dotenv.config()
 
-// cria o servidor
 const app = express()
 
-// 👇 PRIMEIRO: middlewares
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: '*'
 }))
 
 app.use(express.json())
 
-// 👇 DEPOIS: rotas
-app.use('/orders', orderRoutes)
+app.use(orderRoutes)
 
-// rota teste
 app.get('/', (req, res) => {
   return res.json({ message: 'API da Pizzaria rodando 🍕' })
 })
 
-// conecta no banco
 connectDatabase()
 
-// inicia servidor
-app.listen(3000, () => {
-  console.log('Servidor rodando em http://localhost:3000')
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
 })
